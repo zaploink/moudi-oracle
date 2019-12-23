78cd7c45652221614b3e57b0b19577f44fd29705
Kaspar von Gunten <kvgunten@gmx.net>
2019-12-23 15:29:59 +0100

    Select next moudi and amend history (fake mail to console)

--- src/js/oracle.js
+++ src/js/oracle.js
@@ -7,15 +7,26 @@ check (process.argv.length > 2, 'Argument missing: config file must be provided'
 const configFilePath = process.argv[2];
 fs.readFile(configFilePath, 'utf8', (error, data) => {
 	if (error) throw error;
-	organizeNextMoudi(JSON.parse(data));
+	const config = JSON.parse(data);
+	const moudi = determineNextMoudi(config);
+	config.history.push(moudi);
+	fs.writeFile(configFilePath, JSON.stringify(config), error => {
+		if (error) throw error;
+		
+		const recipient = config.organizers.find(organizer => organizer.username == moudi.organizer);
+		log(recipient);
+		sendMail(recipient, moudi);
+	});
 })
 
-function organizeNextMoudi(config) {
+function sendMail(recipient, moudi) {
+	log(`To: ${recipient.email}\n\nHi ${recipient.name},\nyou have to organize next moudi at tramline ${moudi.tramline}.\nGood luck!`);
+}
+
+function determineNextMoudi(config) {
 	const organizer = selectOrganizer(config);
-	log(organizer);
-	// select tram
-	// write history
-	// send email
+	const tramline = selectTramline(config);
+	return { organizer : organizer.username, tramline: tramline };
 }
 
 function selectOrganizer(config) {	
@@ -31,6 +42,15 @@ function selectOrganizer(config) {
 		.indexOf(lastUsername);
 	return config.organizers[(i + 1) % config.organizers.length];
 }
+
+function selectTramline(config) {
+	check(config.history.length < config.tramlines.length, 'Illegal state: No tramlines left for selection');
+	
+	const historyTramlines = config.history.map(record => record.tramline);
+	const candidateTramlines = config.tramlines.filter(line => !historyTramlines.includes(line));
+	return candidateTramlines[Math.floor(Math.random()*candidateTramlines.length)];
+}
+
 
 function log(msg) {
 	if (debug) console.log(msg);
