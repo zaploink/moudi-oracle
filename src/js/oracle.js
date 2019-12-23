d191a3ba6cb3bb883e49c11880ccdda8e4ca90bd
Kaspar von Gunten <kvgunten@gmx.net>
2019-12-23 16:55:03 +0100

    Clean up code

--- src/js/oracle.js
+++ src/js/oracle.js
@@ -10,24 +10,29 @@ fs.readFile(configFilePath, 'utf8', (error, data) => {
 	const config = JSON.parse(data);
 	
 	// check if run this month already unless --force
-	const currentMonth = getCurrentMonth();
 	const force = (process.argv.length > 3 && process.argv[3] == "--force");
-	const lastMoudi = getLastMoudi(config);
-	if (lastMoudi && lastMoudi.month >= currentMonth && !force) {
+	if (shouldRun(config.history) || force) {
+		const moudi = determineNextMoudi(config.organizers, config.tramlines, config.history);
+		config.history.push(moudi);
+		
+		fs.writeFile(configFilePath, JSON.stringify(config), error => {
+			if (error) throw error;
+			
+			const recipient = config.organizers.find(organizer => organizer.username == moudi.organizer);
+			sendMail(recipient, moudi);
+		});
+	} 
+	else {
 		log("Moudi oracle was already run this month. Not doing anything.");
-		return;
 	}
 	
-	const moudi = determineNextMoudi(config);
-	config.history.push(moudi);
-	fs.writeFile(configFilePath, JSON.stringify(config), error => {
-		if (error) throw error;
-		
-		const recipient = config.organizers.find(organizer => organizer.username == moudi.organizer);
-		log(recipient);
-		sendMail(recipient, moudi);
-	});
 })
+
+function shouldRun(history) {
+	const currentMonth = getCurrentMonth();
+	const lastMoudi = getLastMoudi(history);
+	return lastMoudi == null || lastMoudi.month < currentMonth;
+}
 
 function getCurrentMonth() {
 	return getMonth(new Date());
@@ -37,22 +42,25 @@ function getMonth(date) {
 	return `${date.getFullYear()}-${(date.getMonth()+1).toString().padStart(2,'0')}`;
 }
 
-function getLastMoudi(config) {
-	return config.history.length == 0 ? null : config.history[config.history.length-1];
+function getLastMoudi(history) {
+	return history.length == 0 ? null : history[history.length-1];
 }
 
 function sendMail(recipient, moudi) {
 	log(`To: ${recipient.email}\n\nHi ${recipient.name},\nyou have to organize next moudi (${moudi.month}) at tramline ${moudi.tramline}.\nGood luck!`);
 }
 
-function determineNextMoudi(config) {
-	const organizer = selectOrganizer(config);
-	const tramline = selectTramline(config);
+function determineNextMoudi(organizers, tramlines, history) {
+	const lastMoudi = getLastMoudi(history);
+	const nextMonth = calculateNextMonth(lastMoudi);
+	const nextOrganizer = selectOrganizer(lastMoudi, organizers);
+	const nextTramline = selectTramline(tramlines, history);	
 	
-	const lastMoudi = getLastMoudi(config);
-	const nextMonth = lastMoudi ? incrementMonth(lastMoudi.month) : getCurrentMonth();
-	
-	return { month : nextMonth, organizer : organizer.username, tramline: tramline };
+	return { month : nextMonth, organizer : nextOrganizer.username, tramline: nextTramline };
+}
+
+function calculateNextMonth(moudi) {
+	return moudi ? incrementMonth(moudi.month) : getCurrentMonth();
 }
 
 function incrementMonth(month) {
@@ -65,25 +73,25 @@ function incrementMonth(month) {
 	return getMonth(date);
 }
 
-function selectOrganizer(config) {	
-	if (config.history.length == 0) {
+function selectOrganizer(lastMoudi, organizers) {	
+	if (!lastMoudi) {
 		// on first run choose random organizer
-		const i = Math.floor(Math.random() * config.organizers.length);
-		return config.organizers[i];
+		const i = Math.floor(Math.random() * organizers.length);
+		return organizers[i];
 	}
 	
-	const lastUsername = getLastMoudi(config).organizer;
-	const i = config.organizers
+	const lastUsername = lastMoudi.organizer;
+	const i = organizers
 		.map(organizer => organizer.username)
 		.indexOf(lastUsername);
-	return config.organizers[(i + 1) % config.organizers.length];
+	return organizers[(i + 1) % organizers.length];
 }
 
-function selectTramline(config) {
-	check(config.history.length < config.tramlines.length, 'Illegal state: No tramlines left for selection');
+function selectTramline(tramlines, history) {
+	check(history.length < tramlines.length, 'Illegal state: No tramlines left for selection');
 	
-	const historyTramlines = config.history.map(record => record.tramline);
-	const candidateTramlines = config.tramlines.filter(line => !historyTramlines.includes(line));
+	const historyTramlines = history.map(record => record.tramline);
+	const candidateTramlines = tramlines.filter(line => !historyTramlines.includes(line));
 	return candidateTramlines[Math.floor(Math.random()*candidateTramlines.length)];
 }
 
